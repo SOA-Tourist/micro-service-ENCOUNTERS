3079dc98bc66e2f5627abc6bb4901a746a2ea9a5
Pisaric <116560852+Pisaric@users.noreply.github.com>
2024-03-16 19:19:10 +0100

    init project

---
+++ encounters/encounters/src/main/java/com/example/encounters/EncountersApplication.java
@@ -0,0 +1,13 @@
+package com.example.encounters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncountersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncountersApplication.class, args);
+	}
+
+}
---
+++ encounters/encounters/src/test/java/com/example/encounters/EncountersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encounters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncountersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
